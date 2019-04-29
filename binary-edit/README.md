47ea81411ccb673979bcf0dc6084da0091109be9
Jochem on Tuxedo <jochem@example.com>
2019-04-29 12:28:44 +0200

    Adding binary edit

---
+++ binary-edit/README.md
@@ -0,0 +1,14 @@
+# Binary edit
+
+## Readme from one perspective
+The script `asciiedit.sh` is possibly the most nerdy text editor ever. It allows you to type in raw bits, and it'll encode those bits into ASCII for you on the fly. Even backspace is supported, as it is an ASCII character: correct any mistake with a plain and simple `0001000`!
+
+That's it, so all that is left to say is:
+100100011000011110110110010101000001100110111010111011100100001
+
+## Readme from another perspective
+This script revolutionises the way we edit text. It dramatically reduces the amount of keys needed on any keyboard, physical or on touch screens, and we are thrilled to be able to offer you this revolution in typing, today, forever, and for free.
+
+For real: imagine the switch from a massive keyboard that contains all Chinese characters, reduced to a keyboard with only the Latin alphabet. Then imagine making that leap once again, and you'll have the 2- to 3-key\* keyboard necessary for this program.
+
+\*: 3rd key required to ever be able to exit the program ever again once started.
---
+++ binary-edit/asciiedit.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Quit at the first sign of trouble
+set -e
+
+echo "********************************************************" >&2
+echo "* Welcome to the one and only binary ASCII text editor *" >&2
+echo "********************************************************" >&2
+echo >&2
+echo "Type ones and zeroes as long as you want; cancel with any other character or
+key. This script interprets 7-bit ASCII characters, and only 7-bit ASCII
+characters." >&2
+echo "Tip: typed in a wrong character? No worries whatsoever! Simply use the
+backspace ASCII character! Made a mistake in typing the backspace? Just use two
+backspaces characters!" >&2
+echo "This intro is written to stderr, so you can suppress it" >&2
+
+# Exiting happens within the loop
+while [[ 1 ]]; do
+  code=""
+  for i in {1..7}; do
+    read -rn 1 opt
+    if [[ $opt != 0 && $opt != 1 ]]; then
+      # This is admittedly very cryptic: it executes the loop once for every char
+      # in $code.
+      for i in $(eval "echo {0..${#code}}"); do
+        # For some reason, in this case, backspace only puts the 'cursor' back,
+        # doesn't delete the char, so replace by space to mimic actually
+        # deleting.
+        echo -ne "\b \b"
+      done
+      echo
+      exit 0
+    fi
+    code=$code$opt
+  done
+
+  # Same code as above, not very elegant like this (double maintenance)
+  for i in $(eval "echo {1..${#code}}"); do
+    echo -ne "\b \b"
+  done
+
+  # bc is very 'logic', in a way: obase comes after ibase=2, so needs to be
+  # defined in binary: 10000 = 16.
+  hexcode=$(echo "ibase=2;obase=10000;$code" | bc)
+  printf "\x$hexcode"
+done
